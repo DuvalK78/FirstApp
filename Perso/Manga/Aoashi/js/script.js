ae4c26e4eddad488a17fbe03a475633b9154e07e
DuvalK78 <duval.kerian18@ik.me>
2026-06-09 23:45:18 +0200

    Create script.js

---
+++ Perso/Manga/Aoashi/js/script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+
+card.addEventListener('mouseenter', () => {
+
+card.style.transform = 'translateY(-10px)';
+
+});
+
+card.addEventListener('mouseleave', () => {
+
+card.style.transform = 'translateY(0px)';
+
+});
+
+});
